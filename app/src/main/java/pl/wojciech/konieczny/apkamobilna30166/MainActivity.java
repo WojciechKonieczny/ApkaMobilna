61e854e5d88a23261056a7824edbd054de6c0cd0
WojciechKonieczny <wojciech9929@gmail.com>
2022-05-23 22:12:02 +0200

    Implement hamburger menu

--- app/src/main/java/pl/wojciech/konieczny/apkamobilna30166/MainActivity.java
+++ app/src/main/java/pl/wojciech/konieczny/apkamobilna30166/MainActivity.java
@@ -1,14 +1,70 @@
 package pl.wojciech.konieczny.apkamobilna30166;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ImageView imageViewLogoAplikacji;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mainToolbar);
+        // zadeklarowanie szuflady nawigacyjnej
+        // zadeklarowanie stworzonego toolbar_main
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, mainToolbar, R.string.nav_open_hamburger, R.string.nav_close_hamburger);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
-}ss
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch( item.getItemId() ) {
+
+            // identyfikatory przedmiotów z /res/menu/menu_main.xml
+            case R.id.item_cars:
+                Toast.makeText(this, "My cars", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch( id ) {
+            case R.id.item_cars:
+                Toast.makeText(this, "My cars", Toast.LENGTH_LONG).show();
+                break;
+        }
+
+        DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
